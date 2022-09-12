103226fa38666101c4a23ed978ef63f4dd66c0b7
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2022-09-12 14:30:52 -0400

    WIP

--- README.md
+++
@@ -1,1 +0,0 @@
-## Source Code from my [YouTube](https://www.youtube.com/channel/UCmI997WuJrWc5CXeRGqCQlQ/videos) Tutorials
--- Truffle Tutorial for Beginners | Get Started with Truffle/migrations/2_number.js
+++
@@ -1,5 +0,0 @@
-const Number = artifacts.require("Number");
-
-module.exports = function (deployer) {
-  deployer.deploy(Number, 1);
-};
